2e10e49f94e09868273bf875c525ae112684591f
Jayaram <jayaramkasi@pikkol.com>
2018-01-22 11:47:55 +0530

    Simplified util.js

--- src/util.js
+++ src/util.js
@@ -1,36 +1,19 @@
 module.exports = {
   verifyDimensions: (matrix1, matrix2) => {
-    if(!(matrix1 instanceof Array && matrix2 instanceof Array)){
+    if(!(matrix1 instanceof Array && matrix2 instanceof Array))  // Both matrix 1 and 2 are not 2D arrays
       return false;
-    } else if (matrix1.every(el => el instanceof Array)){
-      let rows = matrix1.length;
+    else if (matrix1.every(el => el instanceof Array)){ //If at least matrix 1 is a 2-D array
       let cols = matrix1[0].length;
-
-      if(matrix2.every(el => el instanceof Array) && matrix2.every(el => el.length === cols) && matrix2.length === rows){
-        return true;
-      } else{
-        return false;
-      }
-    } else{
-      if(matrix1.length === matrix2.length){
-        return true;
-      } else{
-        return false;
-      }
-    }
+      // Every element in matrix 2 is an array and has same length as # columns of matrix 1 
+      return (matrix2.every(el => el instanceof Array && el.length === cols) &&   // AND
+        matrix2.length === matrix1.length) ?  //Matrices are of equal length
+        true : false; 
+    } //Else if both are scalars
+    else return matrix1.length === matrix2.length;
   },
-  multDimMatch: (matrix1, matrix2) => {
-    let rows2 = matrix2.length;
-    let cols1 = matrix1.every(el => el instanceof Array) ? matrix1[0].length : matrix1.length;
-
-    return cols1 === rows2;
-  },
-  multRowCol: (row, col) => {
-    let sum = 0;
-    row.forEach((el, idx) => {
-      sum += el * col[idx];
-    });
-
-    return sum;
-  }
+  multDimMatch: (matrix1, matrix2) =>    
+  // if matrix 1 is a 2D Array, return length of first element, or if both are scalars,  check if the lengths match
+    matrix1.every(el => el instanceof Array) ? matrix1[0].length : matrix1.length === matrix2.length,
+  // 
+  multRowCol: (row, col) => row.reduce((accum, el,idx)=> accum += (el * col[idx]) ,0) 
 };
